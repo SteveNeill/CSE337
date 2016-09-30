d5693e7a27fcb5ead52d9e40ed84146fb719b15a
Steven Neill <scneill@oakland.edu>
2016-09-30 13:38:16 -0700

    Changed main() and bottles to not have full implementation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,17 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        bottles();
+       // bottles();
     }
     public static void bottles(){
 
-        for(int i = 99;i>=0;i--) {
-            if(i>1)
-            System.out.println( i + " bottles of beer on the wall," + i +"bottles of beer\n take one down pass it around"
-            + (i-1) + "bottles of beer on the wall "                    );
-            else{
-                System.out.println( 1 + " bottle of beer on the wall," + 1 +" bottle of beer\n take one down pass it around" +
-                        " no more bottles of beer on the wall ");
+      //  for(int i = 99;i>=0;i--) {
+        //    if(i>1)
+          //  System.out.println( i + " bottles of beer on the wall," + i +"bottles of beer\n take one down pass it around"
+            //+ (i-1) + "bottles of beer on the wall "                    );
+            //else{
+              //  System.out.println( 1 + " bottle of beer on the wall," + 1 +" bottle of beer\n take one down pass it around" +
+                     //   " no more bottles of beer on the wall ");
             }
         }
     }
